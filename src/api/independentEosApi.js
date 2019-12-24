b4305b3c6eefd1ba31780757cfa2039ab5a2fec4
shotgunofdeath <faust.child@gmail.com>
2019-12-24 18:22:02 +0200

    getActiveProposals update

--- src/api/independentEosApi.js
+++ src/api/independentEosApi.js
@@ -5,7 +5,7 @@ import config from '@/config';
 const independentEosApi = Eos(config.eos);
 
 const independentEosCalls = {
-  getTableRows(table, code, scope, lowerBound = null, upperBound = null) {
+  getTableRows(table, code, scope, lowerBound = null, upperBound = null, indexPosition = 1) {
     const reqObj = {
       json: true,
       code,
@@ -14,7 +14,7 @@ const independentEosCalls = {
       table_key: '',
       lower_bound: lowerBound,
       upper_bound: upperBound,
-      index_position: 1,
+      index_position: indexPosition,
       key_type: 'i64',
       limit: 1000,
       reverse: false,
