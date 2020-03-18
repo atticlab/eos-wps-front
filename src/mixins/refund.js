920b398069ab0ad40bd9acf5181aa6eb7949c638
mickle <43615363+miclekoby@users.noreply.github.com>
2020-03-18 22:09:44 +0200

    add transactionId field

--- src/mixins/refund.js
+++ src/mixins/refund.js
@@ -33,7 +33,7 @@ export default {
           }]),
           { expireSeconds: 120, blocksBehind: 3 },
         );
-        return res.transaction_id;
+        return res.transaction_id || res.transactionId;
       } catch (e) {
         console.error('$_refund', e);
         this.$errorsHandler.handleError(e);
