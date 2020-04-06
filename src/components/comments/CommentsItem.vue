a4f1f7449d01c2b4b39550fcd46571ee982daae6
theGreatCornholio <faust.child@gmail.com>
2020-04-06 15:05:50 +0300

    A modal window for deletion of a comments is added

--- src/components/comments/CommentsItem.vue
+++ src/components/comments/CommentsItem.vue
@@ -3,16 +3,19 @@
     <div class="font-weight-semi-bold primary-text mb-3">
       {{ author }}
     </div>
+
     <div
       v-if="!isEditing"
       class="mb-6"
     >
       {{ comment }}
     </div>
+
     <CommentsEditor
       v-else
       :initial-comment="comment"
     />
+
     <div>
       <v-btn
         v-if="!isEditing"
@@ -34,14 +37,47 @@
       >
         {{ $t('proposalCreationPage.cancel') }}
       </v-btn>
-      <v-btn
-        small
-        :elevation="0"
-        class="text-transform-none"
-        color="error"
+      <v-dialog
+        v-model="deleteCommentDialog"
+        width="400px"
       >
-        {{ $t('proposalCreationPage.delete') }}
-      </v-btn>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            small
+            :elevation="0"
+            class="text-transform-none mr-3"
+            color="error"
+            v-on="on"
+          >
+            {{ $t('proposalCreationPage.delete') }}
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title>
+            <span class="font-weight-bold">
+              {{ $t('comments.sureToDeleteComment') }}
+            </span>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              class="text-transform-none"
+              color="error"
+              @click="deleteCommentDialog = false"
+            >
+              {{ $t('proposalCreationPage.delete') }}
+            </v-btn>
+            <v-btn
+              class="text-transform-none"
+              color="error"
+              @click="deleteCommentDialog = false"
+            >
+              {{ $t('proposalCreationPage.cancel') }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </div>
   </div>
 </template>
@@ -69,6 +105,7 @@ export default {
   data() {
     return {
       isEditing: false,
+      deleteCommentDialog: false,
     };
   },
   methods: {
